ab949164a1518665f665cc0fb64c61eee86b609c
Emma Hunter <mmrbrts86@gmail.com>
2025-03-13 23:29:09 +0000

    Updated initial city to current location and updated updateCity function to update each second

--- script.js
+++ script.js
@@ -1,36 +1,21 @@
 function updateTime() {
-  let londonElement = document.querySelector("#london");
-  let londonDateElement = londonElement.querySelector("#date");
-  let londonTimeElement = londonElement.querySelector("#time");
-  let londonTime = moment().tz("Europe/London");
+  let currentElement = document.querySelector("#location");
+  if (currentElement) {
+    let currentCityElement = currentElement.querySelector("#cityname");
+    let currentDateElement = currentElement.querySelector("#date");
+    let currentTimeElement = currentElement.querySelector("#time");
+    let timezone = moment.tz.guess();
+    let currentTime = moment().tz(timezone);
 
-  londonDateElement.innerHTML = londonTime.format("dddd, MMMM Do YYYY");
-  londonTimeElement.innerHTML = londonTime.format(
-    "h:mm:ss [<small>]A[</small>]"
-  );
-
-  //   let vancouverElement = document.querySelector("#vancouver");
-  //   let vancouverDateElement = vancouverElement.querySelector("#date");
-  //   let vancouverTimeElement = vancouverElement.querySelector("#time");
-  //   let vancouverTime = moment().tz("America/vancouver");
-
-  //   vancouverDateElement.innerHTML = vancouverTime.format("dddd, MMMM Do YYYY");
-  //   vancouverTimeElement.innerHTML = vancouverTime.format(
-  //     "h:mm:ss [<small>]A[</small>]"
-  //   );
-
-  //   let adelaideElement = document.querySelector("#adelaide");
-  //   let adelaideDateElement = adelaideElement.querySelector("#date");
-  //   let adelaideTimeElement = adelaideElement.querySelector("#time");
-  //   let adelaideTime = moment().tz("Australia/Adelaide");
-
-  //   adelaideDateElement.innerHTML = adelaideTime.format("dddd, MMMM Do YYYY");
-  //   adelaideTimeElement.innerHTML = adelaideTime.format(
-  //     "h:mm:ss [<small>]A[</small>]"
-  //   );
+    currentCityElement.innerHTML = timezone.replace("_", " ").split("/")[1];
+    currentDateElement.innerHTML = currentTime.format("dddd, MMMM Do YYYY");
+    currentTimeElement.innerHTML = currentTime.format("HH:mm:ss");
+  }
 }
 
 function updateCity(event) {
+  clearInterval(timeInterval);
+
   let cityTimezone = event.target.value;
   if (cityTimezone === "current") {
     cityTimezone = moment.tz.guess();
@@ -40,17 +25,23 @@ function updateCity(event) {
   let citiesElement = document.querySelector("#city");
   citiesElement.innerHTML = `
   <div class="city-info">
-      <div class="city-date">
+      <div class="city-name">
         <h2>${cityName}</h2>
         </div>
-      <div class="time">
-        ${cityTime.format("h:mm:ss ")}
-        <small>${cityTime.format("A")}</small></div>
+      <div class="time" id="time">
+        ${cityTime.format("HH:mm:ss ")}
+        </div>
         <div class="date">${cityTime.format("dddd, MMMM Do YYYY")}</div>
       </div>
     </div>`;
   let selectElement = document.querySelector("#selected");
   selectElement.innerHTML = `<a class="refresh" href="/">Refresh</a>`;
+
+  timeInterval = setInterval(() => {
+    cityTime = moment().tz(cityTimezone);
+    citiesElement.querySelector("#time").innerHTML =
+      cityTime.format("HH:mm:ss");
+  }, 1000);
 }
 
 updateTime();
@@ -58,3 +49,25 @@ setInterval(updateTime, 1000);
 
 let citySelectElement = document.querySelector("#cityselect");
 citySelectElement.addEventListener("change", updateCity);
+
+let timeInterval;
+
+//   let vancouverElement = document.querySelector("#vancouver");
+//   let vancouverDateElement = vancouverElement.querySelector("#date");
+//   let vancouverTimeElement = vancouverElement.querySelector("#time");
+//   let vancouverTime = moment().tz("America/vancouver");
+
+//   vancouverDateElement.innerHTML = vancouverTime.format("dddd, MMMM Do YYYY");
+//   vancouverTimeElement.innerHTML = vancouverTime.format(
+//     "h:mm:ss [<small>]A[</small>]"
+//   );
+
+//   let adelaideElement = document.querySelector("#adelaide");
+//   let adelaideDateElement = adelaideElement.querySelector("#date");
+//   let adelaideTimeElement = adelaideElement.querySelector("#time");
+//   let adelaideTime = moment().tz("Australia/Adelaide");
+
+//   adelaideDateElement.innerHTML = adelaideTime.format("dddd, MMMM Do YYYY");
+//   adelaideTimeElement.innerHTML = adelaideTime.format(
+//     "h:mm:ss [<small>]A[</small>]"
+//   );
